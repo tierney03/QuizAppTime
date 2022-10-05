b083cbf35a4da8e81df7a93815cfea9a1a0398c2
tierney03 <tierney_03@mac.com>
2022-10-05 09:45:23 -0500

    Correcting timer syntax

--- assets/index.js
+++ assets/index.js
@@ -24,7 +24,7 @@ function updateCountdown() {
 
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    countdownEl.innerHTML = '$(minutes): $(seconds)';
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
     time--;
 }
 
