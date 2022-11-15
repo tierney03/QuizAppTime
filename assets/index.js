0fe698e1c1aa67e7582803214437e996122df854
tierney03 <tierney_03@mac.com>
2022-11-15 13:30:29 -0600

    Cleaned up with prettier, changed some syntax

--- assets/index.js
+++ assets/index.js
@@ -1,112 +1,112 @@
-const startButton = document.getElementById('start-button')
-const nextButton = document.getElementById('next-button')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const startButton = document.getElementById("start-button");
+const nextButton = document.getElementById("next-button");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
 
 const startingMinutes = 5;
 
-let time = startingMinute = 300;
-const countdownEl = document.getElementById('countdown')
+let time = (startingMinute = 300);
+const countdownEl = document.getElementById("countdown");
 setInterval(updateCountdown, 1000);
 
-let currentQuestionIndex
+let currentQuestionIndex;
 
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
-})
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion(question);
+});
 
 function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    countdownEl.innerHTML = `${minutes}: ${seconds}`;
-    time--;
+  countdownEl.innerHTML = `${minutes}: ${seconds}`;
+  time--;
 }
 
 function startGame() {
-    console.log('Started')
-    startButton.classList.add('hide')
-    currentQuestionIndex = 0
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+  console.log("Started");
+  startButton.classList.add("click");
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion(0);
 }
 
 function setNextQuestion() {
-    showQuestion(question)
+  showQuestion(question);
 }
 
 function showQuestion(question) {
-    questionElement.innerText =question.question
-    question.answers.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerText = answer.text
-        button.classList.add('button')
-        if (answer.correct) {
-            button.dataset.correct = answer.correct
-        }
-        button.addEventListener('click', selectAnswer)
-        answerButtonsElement.appendChild(button)
-    })
+  questionElement.innerText = question[0].question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("button");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
 }
 
 function setStatusClass(element, correct) {
-        clearStatusClass(element)
-        if (correct) {
-            element.classList.add('correct')
-        } else {
-            element.classList.add('wrong')
-        }
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
 }
 
 const question = [
-    {
-        question: 'What does HTML stand for?',
-        answers: [
-            {text: 'Hyper Text Markup Language', correct: true},
-            {text: 'Hot Tango Malasad Licorice', correct: false},
-            {text: 'Hyper Talking Music Latitude', correct: false},
-            {text: 'Hey Tomorrow Means Lazyday', correct: false}
-        ]
-    },
-    {
-        question: 'Where should a CSS file be referenced in a HTML file?',
-        answers: [
-            {text: 'Inside the head section', correct: true},
-            {text: 'Anywhere that feels right', correct: false},
-            {text: 'Inside the footer section', correct: false},
-            {text: 'Inside every div', correct: false}
-        ]
-    },
-    {
-        question: 'Inside which HTML element do we put the Javascript?',
-        answers: [
-            {text: '<js>', correct: false}, 
-            {text: '<div>', correct: false},
-            {text: '<script>', correct: true},
-            {text: '<hot java>>', correct: false}
-        ]
-    },
-    {
-        question: 'How do you create a function in Javascript?',
-        answers: [
-            {text: 'function:myfunction:start', correct: false},
-            {text: 'function = myFunction()', correct: true},
-            {text: '<function class=work></function>', correct: false},
-            {text: 'form over function', correct: false}
-        ]
-    },
-    {
-        question: 'Who is the best student?',
-        answers: [
-            {text: 'Tierney', correct: true},
-            {text: 'Everyone in class', correct: true},
-            {text: 'Everyone who has ever tried to learn coding', correct: true},
-            {text: 'Nobody', correct: false}
-        ]
-    }
-]
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      { text: "Hyper Text Markup Language", correct: true },
+      { text: "Hot Tango Malasad Licorice", correct: false },
+      { text: "Hyper Talking Music Latitude", correct: false },
+      { text: "Hey Tomorrow Means Lazyday", correct: false },
+    ],
+  },
+  {
+    question: "Where should a CSS file be referenced in a HTML file?",
+    answers: [
+      { text: "Inside the head section", correct: true },
+      { text: "Anywhere that feels right", correct: false },
+      { text: "Inside the footer section", correct: false },
+      { text: "Inside every div", correct: false },
+    ],
+  },
+  {
+    question: "Inside which HTML element do we put the Javascript?",
+    answers: [
+      { text: "<js>", correct: false },
+      { text: "<div>", correct: false },
+      { text: "<script>", correct: true },
+      { text: "<hot java>>", correct: false },
+    ],
+  },
+  {
+    question: "How do you create a function in Javascript?",
+    answers: [
+      { text: "function:myfunction:start", correct: false },
+      { text: "function = myFunction()", correct: true },
+      { text: "<function class=work></function>", correct: false },
+      { text: "form over function", correct: false },
+    ],
+  },
+  {
+    question: "Who is the best student?",
+    answers: [
+      { text: "Tierney", correct: true },
+      { text: "Everyone in class", correct: true },
+      { text: "Everyone who has ever tried to learn coding", correct: true },
+      { text: "Nobody", correct: false },
+    ],
+  },
+];
